299d5e9987e57e1bbe5301a576ade306988c22fc
Alyssa Saucedo <Alyssa.Saucedo@teamone-usa.com>
2016-03-02 09:23:22 -0800

    first commit. google auth added. need to add btn

---
+++ google_auth.js
@@ -0,0 +1,8 @@
+var ref = new Firebase("https://luminous-heat-4501.firebaseio.com/");
+ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
